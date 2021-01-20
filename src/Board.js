68b27a65f6e2e61d41efa69e6c2727c3cabf0e2a
ageorgewarren <ageorgewarren@gmail.com>
2021-01-20 17:29:33 -0500

    Complete board through diagonal conflicts

--- src/Board.js
+++ src/Board.js
@@ -189,12 +189,47 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var sum = 0;
+      var colIndex = 0;
+      var startRow = 0;
+      if (minorDiagonalColumnIndexAtFirstRow <= (this.attributes.n - 1)) {
+        startRow = 0;
+        colIndex = minorDiagonalColumnIndexAtFirstRow;
+      } else {
+        startRow = minorDiagonalColumnIndexAtFirstRow - (this.attributes.n - 1);
+        colIndex = this.attributes.n - 1;
+      }
+      for (var i = startRow; i <= this.attributes.n - 1; i++) {
+        var row = this.get(i);
+        if (row[colIndex] === undefined) {
+          break;
+        }
+        sum += row[colIndex];
+        colIndex -= 1;
+      }
+      if (sum > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var flag = false;
+      for (let i = 0; i < ((this.attributes.n - 1) * 2); i++) {
+        flag = this.hasMinorDiagonalConflictAt(i);
+        if (flag) {
+          return flag;
+        }
+      }
+      // for (let i = - 1; i > (this.attributes.n - 1) * -1; i--) {
+      //   flag = this.hasMinorDiagonalConflictAt(i);
+      //   if (flag) {
+      //     return flag;
+      //   }
+      // }
+      return flag;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
